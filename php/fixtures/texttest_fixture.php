53a5faeecd4541cf0cd492ae1b294a313f680651
Chris Vaszauskas <chrisvaszauskas@axiscare.com>
2025-01-16 11:20:28 -0600

    Import code files directly

--- php/fixtures/texttest_fixture.php
+++ php/fixtures/texttest_fixture.php
@@ -2,7 +2,8 @@
 
 declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../src/GildedRose.php';
+require_once __DIR__ . '/../src/Item.php';
 
 use GildedRose\GildedRose;
 use GildedRose\Item;
